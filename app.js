be8dafae6002b766a962c2a3f2847f9c94edad71
DJkhen <92041944+JohnkennethDuenas@users.noreply.github.com>
2023-03-18 17:13:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 500){
+    if(scroll_position > 100){
         header.style.backgroundColor = 'rgb(171, 204, 202)';
     } else{
         header.style.backgroundColor = 'rgba(66, 65, 65, 0.24)';
